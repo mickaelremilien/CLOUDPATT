7c289a8ab8ec2ac3abae85753c1faf511a26ba64
mickaelremilien <mickael.remilien@cloudwatt.com>
2016-05-24 14:43:49 +0200

    collectdG3

--- image-factory/bundle-trusty-collectdG/build.sh
+++ image-factory/bundle-trusty-collectdG/build.sh
@@ -6,4 +6,4 @@ IMG_UBUNTU_TRUSTY="ae3082cb-fac1-46b1-97aa-507aaa8f184f"
 SELF_PATH=`dirname "$0"`
 
 # bundle-build.sh BASENAME CW_BUNDLE_ID BUNDLE_PATH SRC_IMG IMG_OS
-bash "$SELF_PATH/../bundle-build.sh "bundle-trusty-vpn-spawn-server" "VPN" "bundle-trusty-vpn-spawn-server" "$IMG_UBUNTU_TRUSTY" "Ubuntu"
+bash "$SELF_PATH/../bundle-build.sh "bundle-trusty-collectdG" "CGG" "bundle-trusty-collectdG" "$IMG_UBUNTU_TRUSTY" "Ubuntu"
